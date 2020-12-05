60e0e9fe983c27e785c12022affe62a0fa690d63
pagmerek <p.gmerek7@student.uj.edu.pl>
2020-12-05 06:50:46 +0100

    naming tweaks

--- 5/fifth.py
+++ 5/fifth.py
@@ -2,8 +2,7 @@ import math
 with open('input') as f:
     data_input = f.readlines()
     data_input = [x.strip() for x in data_input]
-    row_table = []
-    col_table =[]
+    seat_table = []
     for i in data_input:
         low_row=0
         high_row = 127
@@ -15,21 +14,21 @@ with open('input') as f:
                 high_row=(high_row+low_row)//2
             if i == 'B':  
                 low_row=math.ceil((low_row+high_row)/2)
-            if i=='R':
+            if i== 'R':
                 low_col=math.ceil((high_col+low_col)/2)
-            if i =='L':
+            if i == 'L':
                 high_col = (high_col+low_col)//2
         if(high_row == low_row and high_col == low_col):
-             row_table.append((low_row,low_col))
-    row_table.sort()
+             seat_table.append((low_row,low_col))
+    seat_table.sort()
     seat = (0,0)
     found_num = False
     for i in range(128):
         for j in range(8):
-            if seat in row_table:
+            if seat in seat_table:
                 found_num = True
                 seat = (i,j)
-            elif seat not in row_table and found_num == False:
+            elif seat not in seat_table and found_num == False:
                 seat = (i,j)
             else:
                 print(seat[0]*8 + seat[1])
