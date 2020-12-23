53aa5a4f9feca34eb8726ee1af871cdfa481f85f
pagmerek <p.gmerek@student.uj.edu.pl>
2020-12-23 17:18:16 +0100

    day 23

--- 23/twentythree2.py
+++ 23/twentythree2.py
@@ -1,25 +1,39 @@
-import cProfile
 from collections import deque
 
 def solve(raw):
     cups = [int(x) for x in raw]
     cups += list(range(10,10**6+1))
-    array = dict()
-    for k, v in enumerate(cups):
-        array[v] = cups[(k+1)%1000000]
-    current = 4
-    for _ in range(10000000):
-        splice = [array[current], array[array[current]], array[array[array[current]]]]
-        nexttarget = (current-1)
-        if not nexttarget: nexttarget = 1000000
-        while nexttarget in splice:
-            nexttarget = nexttarget-1
-            if not nexttarget: nexttarget = 1000000
-        array[current] = array[splice[2]]
-        array[splice[2]] = array[nexttarget]
-        array[nexttarget] = splice[0]
-        current = array[current]
-    return array[1], array[array[1]], array[1]*array[array[1]]
+    print(len(cups))
+    current_index = 0
+    cups = deque(cups)
+    for i in range(10000000):
+        if i % 1000 == 0:
+            print(i/10000000 * 100,"%")
+        current_cup = cups[current_index % len(cups)]
+        pick_up = []
+        if(current_index% len(cups) > len(cups)-4):
+            while(current_index% len(cups) != len(cups)-4):
+                cups.rotate(-1)
+                current_index -= 1
+        index = current_index % len(cups)+1
+        for _ in range(3):
+            element = cups[index]
+            del cups[index]
+            pick_up.append(element)
+        destination = current_cup - 1 if current_cup != 1 else max(cups)
+        while(destination in pick_up):
+            destination = (destination-1)
+            if destination == 0:
+                destination = max(cups)
+        destination_index = cups.index(destination)
+        for i in reversed(pick_up):
+            cups.insert(destination_index+1,i)
+        if(destination_index < current_index % len(cups)):
+            current_index += 3
+        current_index += 1
+    while(cups[0] != 1):
+        cups.rotate(-1)
+    print(cups[1]*cups[2])
 
 if __name__ == '__main__':
     with open('input') as f:
